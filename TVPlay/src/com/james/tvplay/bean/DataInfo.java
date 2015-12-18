d6e7d098465c2d7264dc09dfdaf0a36f718ce98d
whatmeever <zzp__2001@sina.com>
2015-12-18 15:46:23 +0800

    first created
    first created

---
+++ TVPlay/src/com/james/tvplay/bean/DataInfo.java
@@ -0,0 +1,172 @@
+package com.james.tvplay.bean;
+
+/**
+ * 解析数据所对应的javabean
+ * @author james
+ *
+ */
+public class DataInfo {
+
+	private String name;
+	private String jumpType;
+	private String subjectId;
+	private String picUrl;
+	private String playUrl;
+	private String icon;
+	private String tag;
+	private String desc;
+	private String videoId;
+	private String hotDegree;
+	private String hotType;
+	private String playTimeIconUrl;
+	private String webUrl;
+	private String splitItem;
+	private String ext;
+	private String libId;
+	private String channelId;
+	private String jumpChannel;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJumpType() {
+		return jumpType;
+	}
+
+	public void setJumpType(String jumpType) {
+		this.jumpType = jumpType;
+	}
+
+	public String getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(String subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public String getPicUrl() {
+		return picUrl;
+	}
+
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+
+	public String getPlayUrl() {
+		return playUrl;
+	}
+
+	public void setPlayUrl(String playUrl) {
+		this.playUrl = playUrl;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getVideoId() {
+		return videoId;
+	}
+
+	public void setVideoId(String videoId) {
+		this.videoId = videoId;
+	}
+
+	public String getHotDegree() {
+		return hotDegree;
+	}
+
+	public void setHotDegree(String hotDegree) {
+		this.hotDegree = hotDegree;
+	}
+
+	public String getHotType() {
+		return hotType;
+	}
+
+	public void setHotType(String hotType) {
+		this.hotType = hotType;
+	}
+
+	public String getPlayTimeIconUrl() {
+		return playTimeIconUrl;
+	}
+
+	public void setPlayTimeIconUrl(String playTimeIconUrl) {
+		this.playTimeIconUrl = playTimeIconUrl;
+	}
+
+	public String getWebUrl() {
+		return webUrl;
+	}
+
+	public void setWebUrl(String webUrl) {
+		this.webUrl = webUrl;
+	}
+
+	public String getSplitItem() {
+		return splitItem;
+	}
+
+	public void setSplitItem(String splitItem) {
+		this.splitItem = splitItem;
+	}
+
+	public String getExt() {
+		return ext;
+	}
+
+	public void setExt(String ext) {
+		this.ext = ext;
+	}
+
+	public String getLibId() {
+		return libId;
+	}
+
+	public void setLibId(String libId) {
+		this.libId = libId;
+	}
+
+	public String getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(String channelId) {
+		this.channelId = channelId;
+	}
+
+	public String getJumpChannel() {
+		return jumpChannel;
+	}
+
+	public void setJumpChannel(String jumpChannel) {
+		this.jumpChannel = jumpChannel;
+	}
+}
