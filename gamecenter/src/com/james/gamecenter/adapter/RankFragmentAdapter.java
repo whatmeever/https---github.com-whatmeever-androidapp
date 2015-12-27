e6aa8c971e7a5c69d75be475a07ba551cea6917e
whatmeever <zzp__2001@sina.com>
2015-12-27 15:10:36 +0800

    data chanaged
    data chanaged

---
+++ gamecenter/src/com/james/gamecenter/adapter/RankFragmentAdapter.java
@@ -0,0 +1,28 @@
+package com.james.gamecenter.adapter;
+
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class RankFragmentAdapter extends FragmentPagerAdapter{
+	
+	private List<Fragment> list = null;
+
+	public RankFragmentAdapter(FragmentManager fm, List<Fragment> list) {
+		super(fm);
+		this.list = list;
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		return list.get(arg0);
+	}
+
+	@Override
+	public int getCount() {
+		return list.size();
+	}
+
+}
